5ce96e881777d1a61bc8f8b304c90213764f42ac
aly <alyelshafei12@gmail.com>
2026-02-03 17:16:27 +0100

    Initial commit

---
+++ basic string.py
@@ -0,0 +1,16 @@
+s1= "Hello World"
+s2='Hello World'
+print(s1)
+print(s2)
+s1=('bdsbvjksbjkvsb')
+print(s1)
+# we can index a string
+s = "banana"
+print(s[1])# indexing starting from zero
+print(s[0])# first letter
+print(s[10//2])
+print(s[-1])
+print(s[-6]) # same as [0]
+
+# len function will let ypu know the amount of characters
+ print(s,len(s))
---
+++ string operations.py
@@ -0,0 +1,5 @@
+# + will perform string concatenation
+s1='hello'
+s2='bye'
+print(s1+s2)
+print(s1+",  "+s2+"!!")
